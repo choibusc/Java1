79db1953546a12f3ab00e004ba8abbb200df43a1
choibusc <2601110303@gspace.kopo.ac.kr>
2026-04-30 11:44:16 +0900

    java for문

---
+++ src/ai0430/ForTest2.java
@@ -0,0 +1,18 @@
+package ai0430;
+
+public class ForTest2 {
+    public static void main(String[] args) {
+
+        int hap = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            hap += i;
+            if(i<10)
+                System.out.print(i + " + ");
+            else
+                System.out.print(i + " = ");
+        }
+
+        System.out.println("1부터 10까지의 합: " + hap);
+    }
+}
